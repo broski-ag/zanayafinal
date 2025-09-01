9d73404ebd93dd257796cadcf2ebc6aae867fb1a
broski-ag <broskiagency@gmail.com>
2025-09-01 15:57:11 +0530

    Add files via upload

---
+++ src/data/religions.ts
@@ -0,0 +1,40 @@
+import { Religion } from '../types';
+
+export const religions: Religion[] = [
+  {
+    id: 'hindu',
+    name: 'Hindu',
+    icon: '🕉️',
+    description: 'Traditional Hindu last rites ceremonies'
+  },
+  {
+    id: 'muslim',
+    name: 'Muslim',
+    icon: '☪️',
+    description: 'Islamic funeral rites and ceremonies'
+  },
+  {
+    id: 'christian',
+    name: 'Christian',
+    icon: '✝️',
+    description: 'Christian funeral services and rites'
+  },
+  {
+    id: 'sikh',
+    name: 'Sikh',
+    icon: '🪯',
+    description: 'Sikh funeral customs and ceremonies'
+  },
+  {
+    id: 'buddhist',
+    name: 'Buddhist',
+    icon: '☸️',
+    description: 'Buddhist funeral rites and ceremonies'
+  },
+  {
+    id: 'jain',
+    name: 'Jain',
+    icon: '🙏',
+    description: 'Jain funeral customs and rituals'
+  }
+];
